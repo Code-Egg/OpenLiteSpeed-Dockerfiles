e44bece0b8365370d96c4be5a185ffa16733078e
lars-hagen <larsh2690@hotmail.com>
2020-02-18 12:54:28 +0100

    More testing

--- 1.6.5/build.sh
+++ 1.6.5/build.sh
@@ -1,14 +1,94 @@
 #!/usr/bin/env bash
+OLS_VERSION=''
+PHP_VERSION=''
+PUSH=''
 
+help_message(){
+    echo 'Command [-ols XX] [-php lsphpXX]'
+    echo 'Command [-ols XX] [-php lsphpXX] --push'
+    echo 'Example: build.sh -ols 1.6.5 -php lsphp73 --push'
+}
 
-docker build . --tag larshagen/playground:$1 --build-arg OLS_VERSION=$1 --build-arg PHP_VERSION=$2
+check_input(){
+    if [ -z "${1}" ]; then
+        help_message
+        exit 1
+    fi
+}
 
-ID=$(docker run -d -p 80:8088 larshagen/playground:$1)
+build_image(){
+    echo $1 $2
+    docker build . --tag litespeedtech/openlitespeed-beta:$1-$2 --build-arg OLS_VERSION=$1 --build-arg PHP_VERSION=$2
+}
 
-set -eux 
-sleep 1
-curl -Ik localhost
+test_image(){
+    ID=$(docker run -d litespeedtech/openlitespeed-beta:$1-$2)
 
-docker kill $ID
+    docker exec -it $ID su -c 'mkdir -p /var/www/vhosts/localhost/html/ && echo "<?php phpinfo();" > /var/www/vhosts/localhost/html/index.php && /usr/local/lsws/bin/lswsctrl restart'
 
-#docker push larshagen/playground:$1
+    HTTP=$(docker exec -it $ID curl -s -o /dev/null -Ik -w "%{http_code}" http://localhost)
+    HTTPS=$(docker exec -it $ID curl -s -o /dev/null -Ik -w "%{http_code}" https://localhost)
+    docker kill $ID
+
+    if [[ "$HTTP" != "200" || "$HTTPS" != "200" ]]; then
+        echo "Test failed, localhost didn't return the right http code"
+        echo "http://localhost" returned "$HTTP"
+        echo "https://localhost" returned "$HTTPS"
+        exit 1
+    fi
+    echo "Tests passed"
+}
+
+push_image(){
+    if [ -f ~/.docker/litespeedtech/config.json ]; then
+        CONFIG=$(echo --config ~/.docker/litespeedtech)
+    else
+        CONFIG=''
+    fi
+    docker ${CONFIG} push litespeedtech/openlitespeed-beta:$1-$2
+}
+
+main(){
+    if [ -z "${OLS_VERSION}" ]; then
+        help_message
+        exit 1
+    fi
+    if [ -z "${PHP_VERSION}" ]; then
+        help_message
+        exit 1
+    fi
+    build_image ${OLS_VERSION} ${PHP_VERSION}
+    test_image ${OLS_VERSION} ${PHP_VERSION}
+    if [ ! -z "${PUSH}" ]; then
+        push_image ${OLS_VERSION} ${PHP_VERSION}
+    fi
+    
+}
+
+check_input ${1}
+while [ ! -z "${1}" ]; do
+    case ${1} in
+        -[hH] | -help | --help)
+            help_message
+            exit 1
+            ;;
+        -ols | -OLS_VERSION | -O) shift
+            check_input "${1}"
+            OLS_VERSION="${1}"
+            ;;
+        -php | -PHP_VERSION | -P) shift
+            check_input "${1}"
+            PHP_VERSION="${1}"
+            ;;
+        --push ) shift
+            PUSH=true
+            ;;            
+        *) 
+            help_message
+            exit 1
+            ;;              
+    esac
+    shift
+done
+
+main
